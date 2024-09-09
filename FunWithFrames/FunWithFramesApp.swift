41a0ce910554b926a08f96443087f4eb1af1b58c
Nour <mohamednour25@gmail.com>
2024-09-09 23:00:01 +0300

    Initial Commit

---
+++ FunWithFrames/FunWithFramesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunWithFramesApp.swift
+//  FunWithFrames
+//
+//  Created by administrator on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct FunWithFramesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
